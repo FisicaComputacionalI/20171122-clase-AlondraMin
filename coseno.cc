0ea27f15468952b38e5a8f4498af5786e64fa0d3
AlondraMin <31377903+AlondraMin@users.noreply.github.com>
2017-11-22 13:29:17 -0600

    Add files via upload
    Trabajo 1

---
+++ coseno.cc
@@ -0,0 +1,11 @@
+//Hacer un programa que calcule la función cos(x) para el punto 0.15,
+//Alondra Dominguez Gonzalez 
+//22 de Noviembre del 2017
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main (){
+  float x = 0.15;
+  float coseno = cos(x);
+
+  cout << "El angulo de 0.15 es" << coseno << endl; return 0; }
